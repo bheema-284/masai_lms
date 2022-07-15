394d5d7c48d55e16c90033a14a000e1a60d9c581
bheema-284 <bheemudug@gmail.com>
2022-07-15 22:51:21 +0530

    some changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-//import Login from "./components/Login";
 import {Signup} from "./components/Signup";
 import { UserAuthContextProvider } from "./context/UserAuthContext";
 import { HomePage } from './components/HomePage';
